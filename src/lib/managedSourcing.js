ce424b40deb0762830132994df09709ac7b52927
Abdulmjeedalmutairi <mjeedalmutairis@gmail.com>
2026-04-08 15:41:48 +0100

    add buildManagedBriefRow

--- src/lib/managedSourcing.js
+++ src/lib/managedSourcing.js
@@ -2,6 +2,8 @@ export function isManagedRequest(request) {
   return request?.type === 'managed' || request?.is_managed === true;
 }
 
+export function buildManagedBriefRow() { return null; }
+
 export async function generateManagedBriefWithAI() {
   return null;
 }
